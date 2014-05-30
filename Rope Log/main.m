f471e0f2c93d7d20e415f65f7b72248b51a7d325
Kyle Johnson <kyle@blueoxpdx.com>
2014-05-29 17:59:36 -0700

    Initial Commit

---
+++ Rope Log/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rope Log
+//
+//  Created by Kyle on 5/29/14.
+//  Copyright (c) 2014 Big Chicken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIGAppDelegate class]));
+    }
+}
---
+++ Rope LogTests/Rope_LogTests.m
@@ -0,0 +1,34 @@
+//
+//  Rope_LogTests.m
+//  Rope LogTests
+//
+//  Created by Kyle on 5/29/14.
+//  Copyright (c) 2014 Big Chicken. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Rope_LogTests : XCTestCase
+
+@end
+
+@implementation Rope_LogTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
